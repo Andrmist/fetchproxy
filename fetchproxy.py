1c154b3f4cc711a33b60b1a925ae8bf8c22208f0
Andriy Cherniy <cherniyandriy@pntl.com.ua>
2022-03-03 12:51:08 +0200

    Added option: http or socks (WIP txt_to_proxychains.py)

--- fetchproxy.py
+++ fetchproxy.py
@@ -5,7 +5,15 @@ from config import api_id, api_hash
 
 parser = argparse.ArgumentParser(description='Fetch proxy from https://t.me/proxy_list_misha')
 parser.add_argument('file_path', metavar='FILE', type=str, help='File path', nargs='?', default='proxy.txt')
+parser.add_argument('--socks', help='fetch socks proxies', action='store_true')
+parser.add_argument('--http', help='fetch http proxies', action='store_true')
 file_path = parser.parse_args().file_path
+socks = parser.parse_args().socks
+http = parser.parse_args().http
+
+if not socks and not http:
+    print("Specify --http or --socks")
+    exit(1)
 
 client = TelegramClient('autoproxy', api_id, api_hash)
 
@@ -16,7 +24,11 @@ def callback(current, total):
 
 
 async def main():
-    last_proxy = [i async for i in client.iter_messages('@proxy_list_misha')][0]
+    last_proxy = None
+    if socks:
+        last_proxy = [i async for i in client.iter_messages('@proxy_list_misha')][0]
+    else:
+        last_proxy = [i async for i in client.iter_messages('@proxy_list_misha')][1]
 
     await client.download_media(last_proxy, file=file_path, progress_callback=callback)
 
---
+++ txt_to_proxychains.py
@@ -0,0 +1,40 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Parse proxy file (fetched from fetchproxy.py) to /etc/proxychains.conf')
+parser.add_argument('proxy_path', metavar='FILE', type=str, help='proxy path', nargs='?', default='proxy.txt')
+parser.add_argument('-c', metavar='config', type=str, help='proxychains config', default='/etc/proxychains.conf')
+parser.add_argument('--socks', help='fetch socks proxies', action='store_true')
+parser.add_argument('--http', help='fetch http proxies', action='store_true')
+
+file_path = parser.parse_args().proxy_path
+proxychains_path = parser.parse_args().c
+socks = parser.parse_args().socks
+http = parser.parse_args().http
+
+proxy_file = open(file_path, 'r')
+if not socks and not http:
+    print("Specify --http or --socks")
+    exit(1)
+
+proxychains_file = open(proxychains_path, 'r+')
+lines = proxychains_file.readlines()
+proxychains_file.seek(0)
+index = 0
+
+try:
+    index = lines.index('[ProxyList]')
+except ValueError:
+    index = len(lines)
+
+proxychains_file.truncate()
+
+for number, line in enumerate(lines):
+    if number not in range(index, -1):
+        proxychains_file.write(line)
+
+for proxy in proxy_file.readlines():
+    host, port = proxy.split(':')
+    proxychains_file.write(f'{"socks5" if socks else "http"} {host} {port}')
+
+proxychains_file.close()
